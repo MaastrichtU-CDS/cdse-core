5e51d127a8d7284db0b08fb13387ee2db2621a17
leroy.linssen <leroy.linssen@maastro.nl>
2021-06-17 10:19:09 +0200

    change app modules names

--- logs/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class LogsTests(TestCase):
-
-    def test_my_first_function(self):
-        self.assertIs(True, True)
--- predictionmodel/apps.py
+++ predictionmodel/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PredictionmodelsConfig(AppConfig):
+class PredictionmodelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'predictionmodels'
+    name = 'predictionmodel'
