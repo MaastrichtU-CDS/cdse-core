221bf2e11757faa1e677b6baecc5a439a70a0cab
Leroy Linssen <leroylinssen@gmail.com>
2021-06-19 22:25:52 +0200

    Automatically assign group to user based on roles in authentication claim clean up code a little

--- authprovider/models.py
+++ authprovider/models.py
@@ -1,34 +1,73 @@
+import logging
+
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
-from .utils import AuthProviderUtils
+from authprovider.utils import AuthProviderUtils
+from django.contrib.auth.models import Group
 
 
-def default_user_settings(user, claims):
-    user.first_name = claims.get('given_name', '')
-    user.last_name = claims.get('family_name', '')
-    user.is_superuser = AuthProviderUtils.check_role_exist(['super_admin'], claims)
-    user.is_staff = AuthProviderUtils.check_role_exist(['it_administrator', 'mdr_regulator', 'medical_professional',
-                                                        'super_admin'], claims)
-    return
+class RolesAndPermissions:
+    GROUPS = {
+        "super_admin": {
+            "log entry": ["add", "delete", "change", "view"],
+            "group": ["add", "delete", "change", "view"],
+            "permission": ["add", "delete", "change", "view"],
+            "user": ["add", "delete", "change", "view"],
+            "content type": ["add", "delete", "change", "view"],
+            "session": ["add", "delete", "change", "view"],
+
+            "datasource": ["add", "delete", "change", "view"],
+            "predictionmodel": ["add", "delete", "change", "view"],
+        },
+        "it_administrator": {
+            "predictionmodel": ["view"],
+        },
+        "mdr_regulator": {
+            "predictionmodel": ["view"],
+        },
+        "medical_professional": {
+            "predictionmodel": ["view"],
+        },
+    }
 
 
 class CustomOIDCAuthenticationBackend(OIDCAuthenticationBackend):
     def create_user(self, claims):
         user = super(CustomOIDCAuthenticationBackend, self).create_user(claims)
-
-        default_user_settings(user, claims)
+        self.default_user_settings(user, claims)
+        self.default_group_settings(user, claims)
         user.save()
-
         return user
 
     def update_user(self, user, claims):
-        default_user_settings(user, claims)
+        self.default_user_settings(user, claims)
+        self.default_group_settings(user, claims)
         user.save()
-
         return user
 
     def verify_claims(self, claims):
         verified = super(CustomOIDCAuthenticationBackend, self).verify_claims(claims)
-        has_required_role = AuthProviderUtils.check_role_exist(['it_administrator', 'mdr_regulator',
-                                                                'medical_professional',
-                                                                'super_admin'], claims)
+        has_required_role = AuthProviderUtils.check_role_exist_in_claim(['it_administrator', 'mdr_regulator',
+                                                                         'medical_professional',
+                                                                         'super_admin'], claims)
         return verified and has_required_role
+
+    @staticmethod
+    def default_user_settings(user, claims):
+        user.first_name = claims.get('given_name', '')
+        user.last_name = claims.get('family_name', '')
+        user.is_superuser = AuthProviderUtils.check_role_exist_in_claim(['super_admin'], claims)
+        user.is_staff = AuthProviderUtils.check_role_exist_in_claim(list(RolesAndPermissions.GROUPS.keys()), claims)
+        return
+
+    @staticmethod
+    def default_group_settings(user, claims):
+        for role in claims.get('realm_access').get('roles', []):
+            if role in list(RolesAndPermissions.GROUPS.keys()):
+                try:
+                    existing_group = Group.objects.get(name=role)
+                    existing_group.user_set.add(user)
+                except Group.DoesNotExist:
+                    logging.warning("Role from claim doesn't exist in groups, please run create_group command "
+                                    "first!")
+                    continue
+
--- authprovider/utils.py
+++ authprovider/utils.py
@@ -1,11 +1,10 @@
-
-
 class AuthProviderUtils:
 
     @staticmethod
-    def check_role_exist(roles: [str], claims) -> bool:
+    def check_role_exist_in_claim(roles: [str], claims) -> bool:
         for role in roles:
             if role in claims.get('realm_access').get('roles', []):
                 return True
 
         return False
+
