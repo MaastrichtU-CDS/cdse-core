88dd126e4f960f1cf1a78e6784c6f8462c979011
Leroy Linssen <leroylinssen@gmail.com>
2021-07-02 20:37:30 +0200

    add fhirendpoint model setup basic form to add, edit and delete endpoints add validation to allow only one default

--- datasource/admin.py
+++ datasource/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from datasource.models import FhirEndpoint
 
-# Register your models here.
+
+class FhirEndpointAdmin(admin.ModelAdmin):
+    list_display = ('name', 'full_url', 'is_default')
+    search_fields = ('name', 'full_url')
+    pass
+
+
+admin.site.register(FhirEndpoint, FhirEndpointAdmin)
--- datasource/models.py
+++ datasource/models.py
@@ -1,3 +1,15 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class FhirEndpoint(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    description = models.CharField(max_length=350)
+    full_url = models.URLField(max_length=2048, null=False, default="http://localhost:1234/fhir")
+    is_default = models.BooleanField(default=False)
+
+    def clean(self):
+        if self.is_default:
+            active = FhirEndpoint.objects.filter(is_default=True)
+            if active.exists():
+                raise ValidationError("An other endpoint is default please disable that one first")
