b11da211191ea9d384991863aca05f9a15447403
Leroy Linssen <leroylinssen@gmail.com>
2021-08-01 12:06:15 +0200

    add some redirect from /prediction to start of wizard

--- predictionmodel/urls.py
+++ predictionmodel/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from predictionmodel.views import StartModelWizard, PrepareModelWizard
 
 urlpatterns = [
+    path("", RedirectView.as_view(url='start/', permanent=True), name="prediction_index"),
     path("start/", StartModelWizard.as_view(), name="prediction_start"),
     path("prepare/", PrepareModelWizard.as_view(), name="prediction_prepare"),
 ]
